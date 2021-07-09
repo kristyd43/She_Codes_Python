1f4b046ad5469355b4182dd43097e1418de57b26
Kristy <kristyd463@gmail.com>
2021-07-10 09:18:12 +1000

    lesson1

---
+++ user_input_playground.py
@@ -0,0 +1,12 @@
+#answer = input("How are you? ")
+
+#message = f"I'm {answer} as well, thnaks for asking!"
+#print(message)
+
+name = input("What is your name? ")
+hobby = input("What is youjr hobby? ")
+print(f"This is {name}, they like {hobby}")
+
+Age = inpit(f"hi {name}, how old are you? ")
+years_until_100 = 100 - int(age)
+print(f"Wow!, hey {name}. did you know you only have {years_until_100} years left until you are 100 years old!?")
---
+++ variables_playground.py
@@ -0,0 +1,54 @@
+#Variables
+day = "Saturday"
+message = f"Today is {day}"
+#print(message)
+#print(type(day))
+month = "January"
+message2 = f"The first {day} in {month} is the best"
+#print(message2)
+
+#Integer Float
+apples = 5
+banana = 8
+fruits = apples + banana
+#print(fruits)
+#print(type(fruits))
+
+#Integers
+#Run distance in m
+run1_dist = 1400
+run2_dist = 1800
+
+#Addition
+total_distance = run1_dist + run2_dist
+#print(total_distance)
+
+#Floats
+#Run distance in km
+run3_dist = 1.7
+run4_dist = 1.35
+
+goal_distance = 3254
+run1_left = goal_distance - run1_dist
+#print(run1_left)
+
+#Addition
+total_distance = run3_dist / run4_dist
+example_distance = run1_dist/run2_dist
+#print(total_distance)
+#print(example_distance)
+
+#Divsion and Multiplication
+example_distance = run1_dist/run2_dist
+#print(run1_dist/1000)
+#print(run3_dist*1000)
+
+run5_dist = "5000"
+##print(run5_dist+3)
+#print(run5_dist +"3")
+#print(run5_dist*3)
+#print(run5_dist*3.0)
+
+print(int(run5_dist)+3)
+print(str(3))
+print(type(str(3)))
